14d4d6c37eec41062b854c850944c4c05b2ec184
Daniel Brazel <dbrazel4@gmail.com>
2018-01-12 20:27:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(artists, instruments){
+  var emptyArray = []
+  for(var i = 0; i < artists.length; i++){
+    emptyArray.push(artists[i] + " plays " + instruments[i])
+  }
+  return emptyArray
+}
+
+function johnLennonFacts(facts){
+  var n = 0
+  lennonFactsArray = []
+  while(n < facts.length){
+    lennonFactsArray.push()
+    n++
+  }
+  return lennonFactsArray
+}
